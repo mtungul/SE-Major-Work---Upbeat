342f8babcc09dbc1befc6dea733b45a74cf722e4
mtungul <mtungul@parrastu.catholic.edu.au>
2026-04-07 17:42:59 +1000

    add home buttons

--- startScreen.py
+++ startScreen.py
@@ -20,55 +20,66 @@ select = pygame.image.load(os.path.join(current_dir, 'img', 'select.png')).conve
 logo = pygame.image.load(os.path.join(current_dir, 'img', 'logo.png')).convert_alpha()
 
 #resize images
-home_btn_normal_size = (325, 200)
-home_btn_hover_size = (358, 220)
-pitch_btn_normal = pygame.transform.smoothscale(pitch_btn, home_btn_normal_size)
-pitch_btn_hover = pygame.transform.smoothscale(pitch_btn, home_btn_hover_size)
-rhythm_btn = pygame.transform.scale(rhythm_btn, (325, 200))
 select = pygame.transform.scale(select, (170, 50))
 logo = pygame.transform.scale(logo, (460, 150))
 
 #text_surface = font.render('Hello, Pygame!', True, (255, 255, 255)) # text, true, text colour
 
-def pitchBtn():
-    global pitch_btn
-    mouse_pos = pygame.mouse.get_pos()
-    mouse_pressed = pygame.mouse.get_pressed()
-    pitch_btn_rect = pygame.Rect(600, 300, 325, 200) #creates rectangle at (600, 300) with dimensions 325x200px
-    if pitch_btn_rect.collidepoint(mouse_pos):
-        image = pitch_btn_hover
-        draw_pos = (583, 290)
-    else:
-        image = pitch_btn_normal
-        draw_pos = (600, 300)
+def open_pitch_levels():
+    print("Opening pitch levels")
 
-    screen.blit(image, draw_pos)
+def open_rhythm_levels():
+    print("Opening rhythm levels")
 
-    if pitch_btn_rect.collidepoint(mouse_pos) and mouse_pressed[0]:
-        screen.fill((0, 0, 0))
-        pygame.display.update()
+class Button:
+    def __init__(self, x, y, image, size, action=None):
+        self.original = image
+        self.normal = pygame.transform.smoothscale(image, size)
+
+        hover_size = (int(size[0]*1.1), int(size[1]*1.1))
+        self.hover = pygame.transform.smoothscale(image, hover_size)
+
+        self.rect = self.normal.get_rect(topleft=(x, y))
+        self.hover_rect = self.hover.get_rect(center=self.rect.center)
+
+        self.action = action
+    
+    def draw(self, screen):
+            mouse_pos = pygame.mouse.get_pos()
+
+            if self.rect.collidepoint(mouse_pos):
+                screen.blit(self.hover, self.hover_rect)
+            else:
+                screen.blit(self.normal, self.rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if self.rect.collidepoint(event.pos):
+                if self.action:
+                    self.action()
+
+pitch_button = Button(600, 300, pitch_btn, (325, 200), open_pitch_levels)
+rhythm_button = Button(150, 300, rhythm_btn, (325, 200), open_rhythm_levels)
+
 
 # Main loop
 run = True
 while run:
-    screen.fill((255, 255, 255))
-    screen.blit(logo, (315, 50))
-    pitchBtn()
-    screen.blit(rhythm_btn, (150, 300))
-    screen.blit(select, (450, 200))
     
     for event in pygame.event.get():
+        screen.fill((255, 255, 255))
+        screen.blit(logo, (315, 50))
+        screen.blit(select, (450, 200))
+        rhythm_button.draw(screen)
+        rhythm_button.handle_event(event)
+        pitch_button.draw(screen)
+        pitch_button.handle_event(event)
+
         if event.type == pygame.QUIT:
             run = False
 
-    
-    #screen.fill((0, 50, 0)) # Fill screen with black
-
-
 
     pygame.display.update()
-
-
 
 
 pygame.quit()
